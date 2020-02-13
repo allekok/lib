3915c75c8f693ec928b716409b143d77e3b2f3fe
Payam Bapiri <payambapiri.97@gmail.com>
2020-02-13 11:06:26 +0330

    Organize project files

--- server/constants-sample.php
+++ server/constants-sample.php
@@ -8,8 +8,8 @@ const SQL_TABLES = ["en" , "fa"];
 const SQL_TABLE_DEF = "fa";
 
 /* Location for tree directory */
-const DEST_PATH = "tree";
+const DEST_PATH = "../tree";
 
 /* Location for version file */
-const VERSION_PATH = "VERSION";
+const VERSION_PATH = "../VERSION";
 ?>
